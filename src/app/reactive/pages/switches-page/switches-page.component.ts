74986a32a371645ea08da7a745895bd5d88fcfe3
diego <sdieg82@gmail.com>
2024-08-23 09:41:36 -0500

    register form

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -6,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class SwitchesPageComponent {
 
+  public myForm:FormGroup
+
+
+  constructor (private fb:FormBuilder ){
+  this.myForm=this.fb.group({
+  gender:['M',Validators.required],
+  wantNotifications:[true,Validators.required],
+  termsAndConditions:[false,Validators.requiredTrue]
+  })
+  }
+  // onsubmit
+  onSave():void{
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+  }
+
+  
+  isValidField(field:string):boolean | null{
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched
+
+  }
+
+  isValidFieldInArray(formArray:FormArray, index:number):boolean | null{
+    return formArray.controls[index].errors
+    && formArray.controls[index].touched
+
+  }
 }
